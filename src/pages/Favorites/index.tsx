68f9d796a34af9e8be6672f4b53d6a043cbd9365
Thiago Bignotto <bignotto@gmail.com>
2020-10-17 07:50:42 -0300

    all tests passed

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -33,6 +33,17 @@ const Favorites: React.FC = () => {
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
       // Load favorite foods from api
+      try {
+        const response = await api.get('/favorites');
+        setFavorites(
+          response.data.map((f: Food) => {
+            return {
+              ...f,
+              formattedValue: formatValue(f.price),
+            };
+          }),
+        );
+      } catch (error) {}
     }
 
     loadFavorites();
